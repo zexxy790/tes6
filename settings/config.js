d3740a566b333c3017406e835ec330d63e695c69
zexxy790 <zexxy40@gmail.com>
2025-07-17 23:51:31 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,4 @@
-
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6281519671381']
+global.gambar = "https://files.catbox.moe/5gp130.jpg"
+// GANTI NO OWNER DENGAN NOMOR MU LALU RUNN SEPERTI BIASA !!!
